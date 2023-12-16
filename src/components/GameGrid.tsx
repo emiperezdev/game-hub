63d5be88c95d78d16c14f59364d19550d22439cf
Emiliano Perez <emiperez.dev@gmail.com>
2023-12-15 18:17:56 -0600

    Display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,8 +12,8 @@ export const GameGrid = () => {
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={10}
-        padding="10"
+        padding="3"
+        spacing={3}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
