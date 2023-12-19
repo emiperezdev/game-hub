0ec9a07f0e1ee92d681bff09cd28812fb3dbe102
Emiliano Perez <emiperez.dev@gmail.com>
2023-12-19 12:01:23 -0600

    Build search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,14 @@
-import { HStack, Image } from '@chakra-ui/react'
-import logo from '../assets/logo.webp';
-import { ColorModeSwitch } from './ColorModeSwitch';
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+import { ColorModeSwitch } from "./ColorModeSwitch";
+import { SearchInput } from "../assets/SearchInput";
 
 export const NavBar = () => {
   return (
-    <HStack justifyContent={'space-between'} padding='10px'>
-      <Image src={logo} boxSize='60px'/>
+    <HStack padding="10px">
+      <Image src={logo} boxSize="60px" />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
-  )
-}
+  );
+};
