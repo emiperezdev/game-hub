c885520c7c39279639bffe090cc1e67a1391d0f4
Emiliano Perez <emiperez.dev@gmail.com>
2023-12-26 16:04:25 -0600

    Removing SearchInput from assets to components folder

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import { ColorModeSwitch } from "./ColorModeSwitch";
-import { SearchInput } from "../assets/SearchInput";
+import { SearchInput } from "./SearchInput";
 
 interface Props {
   onSearch: (searchText: string) => void;
